9db3c98a36a40112392736b5e9bf9e8ad8568376
Michel Manarin <michelmanarin@gmail.com>
2024-01-25 20:15:54 -0300

    git push

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://admin:ofK3cT72g3NoHxy@estudos.8eh6ljx.mongodb.net/?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || 'uTk#L@=+3r34m'
+  jwtSecret: process.env.JWT_SECRET
 }
